d88dc9d2ba90ae0159f735743644986af261d206
KerryAndres <bustos.andres@correounivalle.edu.co>
2024-11-17 16:28:01 -0500

    Se agrega consulta para traer todas las reservas

--- rutas/reservas.js
+++ rutas/reservas.js
@@ -1,4 +1,23 @@
-const router = require("express").Router();
-const pool = require('../database');
+const router = require("express").Router()
+const pool = require('../database')
 
-router.get("/", async (req, res) => {})
+router.get("/", async (req, res) => {
+  
+    try {
+        // Consulta SQL para obtener las reservas
+        const query = 'SELECT * FROM reservas';
+            
+        // Ejecutar la consutla
+        const reservas = await pool.query(query);
+    
+        // Envía la respuesta con las reservas encontrados
+        res.json(reservas.rows);
+    
+    } catch (error) {
+        console.error('Error al obtener las reservas:', error);
+        res.status(500).json({ error: 'Error al obtener reservas' });
+    }
+  
+});
+
+module.exports = router;
